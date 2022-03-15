8267d8060af6633076614a9a1f3a1276064f4f0b
Wisam <wisamshikhkhalil555@gmail.com>
2022-03-15 09:34:44 +0100

    Start

---
+++ Github/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Github
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] items = Regex.Split(textBox1.Text, "\r\n");
+            listBox1.Items.AddRange(items);
+        }
+    }
+}
